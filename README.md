039127e84112ad7ac21212898f6d689d5410c7a9
julienrdt <julien.roduit@hes-so.ch>
2026-09-20 23:44:13 +0200

    Add retries and idempotence demo

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
 # 302-Retries-and-idempotence
+
+## Description
+
+This repository is a demo to show the concepts of retries, backoff and idempotence when running Flyte task. To have a concrete exemple, the project use a synthetic healthcare dataset [`healthcare_dataset`](https://www.kaggle.com/datasets/prasad22/healthcare-dataset)
+
+The demo intentionally introduces random task failures to observe how Flyte handles retries and how backoff affects the delay between retry attemps. It also demonstrates the difference between idempotent and non-idempotent.
+
+## Project structure
+```
+302-Retires-and-idempotence
+├── data/
+│   └── healtcare_dataset.csv      # Concrete data
+├── output/
+│   └── logs.txt                   # Temporary file for tasks
+│   └── result.txt                 # The final result with all tasks
+├── src/
+│   └── main.py                    # The script where tasks are execute
+├── .gitignore
+├── .python-version                # The python version
+├── pyproject.toml                 # Project dependencies
+├── README.md                      # The instruction
+└── uv.lock
+```
+
+# Requierments
+
+- Python 3.13
+- uv
+
+# Setup
+
+Clone the repository
+```
+git clone https://github.com/rdtjulien/302-Retries-and-idempotence
+```
+
+To go in the folder
+```
+cd 302-Retries-and-idempotence
+```
+
+Install the dependencies
+```
+uv sync
+```
+
+To run the project
+```
+uv run src/main.py
+```
+
+# Result
+
+The project runs several Flyte tasks with different configurations.
+
+The results are written to `output/result.txt`
---
+++ src/main.py
@@ -0,0 +1,118 @@
+import random
+import flyte
+import pandas as pd
+from datetime import timedelta
+import logging
+
+# Create a Flyte task environment
+env = flyte.TaskEnvironment(
+    name="retry-idempotent"
+)
+
+# Simulate a task that read, process and write data
+async def data(idempotent):
+
+    path = "data/healthcare_dataset.csv"
+    df = pd.read_csv(path)
+
+    new_df = df.head()
+
+    # Overwrite mode for idempotent tasks and append mode for non-idempotent tasks
+    m = "a"
+    if idempotent:
+        m = "w"
+
+    with open("output/logs.txt", m) as f:
+        f.write(new_df.to_string())
+        f.write("\n\n")
+
+        x = random.randint(0, 1)
+
+        # Simulate a task failure with a 50% chance
+        if x == 0:
+            print("Task fails")
+            f.write("Task fails\n")
+            raise Exception("Task fails")
+
+        print("Task success")
+
+# Define Flyte tasks with different configuration
+@env.task
+async def no_policy():
+    await data(False)
+
+
+@env.task(
+    retries=flyte.RetryStrategy(
+        count=3,
+    ),
+)
+async def policy():
+    await data(False)
+
+
+@env.task(
+    retries=flyte.RetryStrategy(
+        count=3,
+        backoff=flyte.Backoff(
+            base=timedelta(seconds=2),
+            factor=2.0,
+            cap=timedelta(minutes=5),
+        ),
+    ),
+)
+async def policy_backoff():
+    await data(False)
+
+
+@env.task(
+    retries=flyte.RetryStrategy(
+        count=3,
+    ),
+)
+async def policy_idempotent():
+    await data(True)
+
+
+@env.task(
+    retries=flyte.RetryStrategy(
+        count=3,
+        backoff=flyte.Backoff(
+            base=timedelta(seconds=2),
+            factor=2.0,
+            cap=timedelta(minutes=5),
+        ),
+    ),
+)
+async def policy_idempotent_backoff():
+    await data(True)
+
+# Run all tasks and collect the result
+@env.task
+async def main():
+
+    open("output/result.txt", "w").close()
+
+    for t in [no_policy,policy,policy_backoff,policy_idempotent,policy_idempotent_backoff]:
+        name = t.name.split(".")[-1]
+
+        print(f"\n===== {name} =====")
+        try:
+            await t()
+        except Exception:
+            pass
+
+        with open('output/logs.txt','r') as logs, open('output/result.txt','a') as result:
+            result.write(name)
+            result.write("\n\n")
+            for line in logs:
+                    result.write(line)
+            result.write("\n\n\n\n")
+
+        open("output/logs.txt", "w").close()
+
+
+if __name__ == "__main__":
+    # Hide flyte log
+    flyte.init(log_level=logging.CRITICAL)
+    flyte.run(main)
